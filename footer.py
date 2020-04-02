b120f239e75964e4b8024479aabcaa40599cf51b
AjxLab <abe12@mccc.jp>
2020-04-03 06:06:38 +0900

    exit footer

---
+++ footer.py
@@ -0,0 +1,24 @@
+import time
+
+START_TIME = time.time()
+
+
+def footer_exit(status=0):
+    ## -----*----- EXIT -----*----- ##
+    print("")
+    print("")
+    print("")
+    print("----------------------------------------------------------------------")
+    print("Ran {} {} in {:5.3f}s".format(
+        status,
+        __file__,
+        time.time() - START_TIME
+    ))
+    print("")
+    if status==0:
+        print("\033[32mOK\033[0m")
+    else:
+        print("\033[31mERROR\033[0m")
+
+    exit(status)
+
